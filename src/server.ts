0be957a57c42a3e97b187528e62109197bbf85a2
Lucas Dias <luucasdias@icloud.com>
2024-06-26 11:10:42 -0300

    teste 2

--- src/server.ts
+++ src/server.ts
@@ -44,6 +44,7 @@ app.get('/hello', async () => {
 
 
 app.listen({port: env.PORT,
+        host: ("RENDER" in process.env) ? '0.0.0.0' : 'localhost',
 }).then(()=>{
     console.log('HTTP Server Running!')
 })
